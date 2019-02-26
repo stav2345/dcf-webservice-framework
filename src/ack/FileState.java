7ae5bbde33eb89c930d9ba4cc4acd8f813259c4b
shahaal <Alban.SHAHAJ@ext.efsa.europa.eu>
2019-02-26 09:57:43 +0100

    Added Maven + solved memory leaks and other improvements

--- src/ack/FileState.java
+++ src/ack/FileState.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ack;
 
 import soap.GetAck;
@@ -51,57 +50,3 @@ public enum FileState {
 		return OTHER;
 	}
 }
-=======
-package ack;
-
-import soap.GetAck;
-
-/**
- * Response of the {@link GetAck} request.
- * @author avonva
- *
- */
-public enum FileState {
-	
-	READY("READY"),
-	WAIT("WAIT"),
-	FAIL("FAIL"),
-	ACCESS_DENIED("ACCESS_DENIED"),
-	EXCEPTION("EXCEPTION"),
-	OTHER("OTHER");
-	
-	private String headerName;
-	
-	/**
-	 * Initialize the enumerator with the real 
-	 * header name that is present in the xlsx
-	 * @param headerName
-	 */
-	private FileState(String headerName) {
-		this.headerName = headerName;
-	}
-	
-	/**
-	 * Get the header name related to the enum field
-	 * @return
-	 */
-	public String getHeaderName() {
-		return headerName;
-	}
-
-	/**
-	 * Get the enumerator that matches the {@code text}
-	 * @param text
-	 * @return
-	 */
-	public static FileState fromString(String text) {
-		
-		for (FileState b : FileState.values()) {
-			if (b.headerName.equalsIgnoreCase(text)) {
-				return b;
-			}
-		}
-		return OTHER;
-	}
-}
->>>>>>> 5db4cda62aab4fbc1c8dbec8998c82e8bc1d4475
--- src/user/IDcfUser.java
+++ src/user/IDcfUser.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package user;
 
 public interface IDcfUser {
@@ -15,28 +14,3 @@ public interface IDcfUser {
 	 */
 	public String getPassword();
 }
-=======
-package user;
-
-public interface IDcfUser {
-
-	/**
-	 * Get the user name of the user
-	 * @return
-	 */
-	public String getUsername();
-	
-	/**
-	 * Get the password of the user
-	 * @return
-	 */
-	public String getPassword();
-	
-	/**
-	 * Get the open API token identifying 
-	 * an user without username and password
-	 * @return
-	 */
-	public String getToken();
-}
->>>>>>> 5db4cda62aab4fbc1c8dbec8998c82e8bc1d4475
